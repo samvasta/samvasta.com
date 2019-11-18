f8f83f762fb1260437085add9c1066dcaa5deb12
Sam Vasta <samuel.m.vasta@gmail.com>
2019-11-17 22:12:07 -0600

    Added tinkering content

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var server = express();
 
 //Middleware
 server.set("view engine", "ejs");
-server.set("views", path.join(__dirname, "views"), path.join(__dirname, "views/procgen"));
+server.set("views", path.join(__dirname, "views"), path.join(__dirname, "views/procgen"), path.join(__dirname, "views/tinkering"));
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: false}));
@@ -19,11 +19,12 @@ server.use(express.static(path.join(__dirname, "static/js")));
 //Routes
 server.get("/", GET_index);
 server.get("/portfolio", GET_portfolio);
-server.get("/procgen", GET_procgen)
+server.get("/procgen", GET_procgen);
 server.get("/procgen/:page", GET_procgen_details);
 
 server.get("/tinkering", GET_tinkering)
-server.get("/art", GET_art)
+server.get("/tinkering/:page", GET_tinkering_details);
+server.get("/art", GET_art);
 
 //Start server
 server.listen(80, '0.0.0.0', function(){
@@ -51,6 +52,10 @@ function GET_procgen_details(req, res){
 
 function GET_tinkering(req, res){
     res.render("tinkering", {pageName: "tinkering", title: "Tinkering Projects"});
+}
+
+function GET_tinkering_details(req, res){
+  res.render("tinkering/" + req.params.page, {pageName: "tinkering", 'tinkering_name':req.params.page, title:"Tinkering Projects"});
 }
 
 function GET_art(req, res){
